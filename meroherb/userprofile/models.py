c5434221acc886fea675522246730ff9f460aa8f
nishant1100 <nishantstha1010@gmail.com>
2024-01-18 18:08:05 +0545

    editprofile backend

--- meroherb/userprofile/models.py
+++ meroherb/userprofile/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.forms import PasswordChangeForm
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=150)
+    email = models.EmailField()
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    # Add additional fields for the user profile (e.g., phone_number, etc.)
--- meroherb/userprofile/views.py
+++ meroherb/userprofile/views.py
@@ -1,5 +1,39 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import PasswordChangeForm
+from .forms import UserProfileForm
+from .models import UserProfile
+from django.contrib import messages
+
+@login_required
 def userprofile(request):
-    return render(request,'userprofile/userprofile.html')
+    user = request.user
+    user_profile = UserProfile.objects.get(user=request.user)
+
+    user_profile_form = UserProfileForm(request.POST or None, request.FILES or None, instance=user_profile)
+    password_change_form = PasswordChangeForm(user)
+
+    print("Request method:", request.method)
+
+    if request.method == 'POST':
+        if 'user-details-form-submit' in request.POST:
+            user_profile_form = UserProfileForm(request.POST, instance=user)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+                messages.success(request, 'User details updated successfully.')
+
+        elif 'password-change-form-submit' in request.POST:
+            print("Processing password change form")
+            # Process password change form
+            password_change_form = PasswordChangeForm(user, request.POST)
+            if password_change_form.is_valid():
+                password_change_form.save()
+                print("Password change form is valid. Redirecting...")
+        return redirect('userprofile:editprofile')
+    else:
+        form = UserProfileForm(instance=user)
+        password_change_form = PasswordChangeForm(request.user)
+
+    print("Rendering userprofile.html")
+    return render(request, 'userprofile/userprofile.html', {'user': user, 'user_profile_form': user_profile_form, 'password_change_form': password_change_form})
