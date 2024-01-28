9088ecb977a33fce3f434b30cc311ba8d4634114
Ujjwols <shrestha.ujjwol1@gmail.com>
2024-01-28 22:01:24 +0545

    forgert pass

--- meroherb/meroherb/settings.py
+++ meroherb/meroherb/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Meroherbs',
         'USER': 'postgres',
-        'PASSWORD': 'postgres@123',
+        'PASSWORD': '21013002',
         'HOST': 'localhost',  # Replace with your PostgreSQL server's address if necessary
         'PORT': '',          # Leave empty to use the default PostgreSQL port (usually 5432)
     }
