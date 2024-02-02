421f36a93d54858caa689c4e973f42f0c6cd774c
nishant1100 <nishantstha1010@gmail.com>
2024-02-02 10:33:14 +0545

    user profile fix and UI changes

--- meroherb/meroherb/settings.py
+++ meroherb/meroherb/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Meroherbs',
         'USER': 'postgres',
-        'PASSWORD': '21013002',
+        'PASSWORD': 'password',
         'HOST': 'localhost',  # Replace with your PostgreSQL server's address if necessary
         'PORT': '',          # Leave empty to use the default PostgreSQL port (usually 5432)
     }
